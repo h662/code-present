2afeea2b3c4c7175fb5aeaf6f31e54b0bcb2537e
h662 <h662hong@gmail.com>
2025-06-15 09:26:26 +0900

    feat(config): cors config (allowedOrigins)

--- src/main/java/com/example/codepresent/config/WebConfig.java
+++ src/main/java/com/example/codepresent/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://localhost:5173", "https://redundant-nanny-h662-43ed6282.koyeb.app")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
